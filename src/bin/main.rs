4739252fd3bd3c8f0e815ba61e97687c61d5e067
elmica <mb.codingtest@gmail.com>
2024-12-26 21:49:30 -0500

    added test file

---
+++ src/bin/main.rs
@@ -0,0 +1,5 @@
+use printer_listener::add;
+
+fn main() {
+    println!("2 + 3 = {}", add(2, 3));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+//use tokio::sync::Notify;
+//use std::sync::Arc;
+
 // Adds 2 numbers
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-//https://github.com/elmica/printout-listener.git
+ 
